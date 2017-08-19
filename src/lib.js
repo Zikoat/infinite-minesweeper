8fce2d020d99ed175314036215112b2bf975c719
Sigurd skole-pc <sschoeler99@gmail.com>
2017-08-19 13:43:35 +0200

    added pixi as a dependency and test lib

---
+++ src/lib.js
@@ -0,0 +1,10 @@
+/**
+ * Created by sisc0606 on 19.08.2017.
+ */
+export const sqrt = Math.sqrt;
+export function square(x) {
+	return x * x;
+}
+export function diag(x, y) {
+	return sqrt(square(x) + square(y));
+}
--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,7 @@ module.exports = {
 	plugins: [new HtmlWebpackPlugin({
 			title: "change this meme"
 		})],
+	devtool: "inline-source-map",
 	devServer: {
 		contentBase: "./dist"
 	}
