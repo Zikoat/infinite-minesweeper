ce5db07006a74eb707461329a17e2bf1755dc653
Sigurd skole-pc <sschoeler99@gmail.com>
2018-04-07 15:42:30 +0200

    made the cursor extend eventemitter

--- src/Cursor.js
+++ src/Cursor.js
@@ -1,14 +1,14 @@
-class Cursor {
+import EventEmitter from 'eventemitter3';
+
+export default class Cursor extends EventEmitter {
 	constructor(x = 0, y = 0) {
+		super();
 		this.x = x;
 		this.y = y;
 	}
 	moveTo(x, y) {
 		this.x = x;
 		this.y = y;
-		// send move event
-	}
-	displace(dx, dy) {
-		this.moveTo(this.x + dx, this.y + dy);
+		this.emit('move', x, y);
 	}
 }
