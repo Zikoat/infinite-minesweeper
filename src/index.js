4f70bde3a8b0cd56938d4116a7190b73405882fe
Sigurd skole-pc <sschoeler99@gmail.com>
2018-04-11 23:21:42 +0200

    cleaned up dependencies

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import _ from 'lodash';
 import Field from "./Field";
 import FieldRenderer from "./FieldRenderer";
 import FieldStorage from "./FieldStorage";
@@ -7,7 +6,6 @@ import css from "./css/stylesheet.css";
 
 if (localStorage.getItem('defaultSavedField')) {
 	self.f = FieldStorage.load('defaultSavedField');
-	console.log(f);
 	//console.log(`loading previous field with ${open} mines opened`, self.f.getAll().filter((cell)=>cell.isOpen).length);
 } else {
 	self.f = new Field(0.20, 3);
@@ -17,4 +15,5 @@ if (localStorage.getItem('defaultSavedField')) {
 self.renderer = new FieldRenderer(f);
 self.FieldStorage = FieldStorage;
 
+// todo
 // f.on("changedCells", ()=>FieldStorage.save(f, 'defaultSavedField'));
