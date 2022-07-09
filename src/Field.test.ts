b823bf4ca6d33181ff4d0188cc7c321eba05b10c
Zikoat <sschoeler99@gmail.com>
2022-07-09 21:05:38 +0200

    Group tests in suites

--- src/Field.test.ts
+++ src/Field.test.ts
@@ -1,34 +1,49 @@
 // tests/demo.js
-import { test } from 'uvu';
-import * as assert from 'uvu/assert';
-import Cell from './Cell';
-import Field from './Field';
+import { suite, test } from "uvu";
+import * as assert from "uvu/assert";
+import Cell from "./Cell";
+import Field from "./Field";
 
-test('Math.sqrt()', () => {
+test("Math.sqrt()", () => {
   assert.is(Math.sqrt(4), 2);
   assert.is(Math.sqrt(144), 12);
   assert.is(Math.sqrt(2), Math.SQRT2);
 });
 
-test('JSON', () => {
+test("JSON", () => {
   const input = {
-    foo: 'hello',
-    bar: 'world'
+    foo: "hello",
+    bar: "world",
   };
 
   const output = JSON.stringify(input);
 
   assert.snapshot(output, `{"foo":"hello","bar":"world"}`);
-  assert.equal(JSON.parse(output), input, 'matches original');
+  assert.equal(JSON.parse(output), input, "matches original");
 });
 
-test("Field",()=>{
-    const field = new Field()
-    assert.is(field.gameOver, false)
-})
-test("cell",()=>{
-    const cell = new Cell(0,0,undefined, false,false)
-    assert.is(cell.isOpen, false)
-})
+const field = suite("Field");
+
+field("should be able to be constructed", () => {
+  const field = new Field();
+  assert.is(field.gameOver, false);
+});
+
+field("toJson should return the field as an object", () => {
+  const field = new Field();
+  const json = field.toJSON();
+  assert.equal(json, {
+    probability: 0.5,
+    score: 0,
+  });
+});
+
+const cell = suite("Cell");
+
+cell("should be able to be instantiated", () => {
+  const cell = new Cell(0, 0, undefined, false, false);
+  assert.is(cell.isOpen, false);
+});
 
 test.run();
+field.run();
