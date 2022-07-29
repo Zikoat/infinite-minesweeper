951b821d866833ca2b816b5b149fb938f0f35d3d
prompemannen <sschoeler99@gmail.com>
2022-07-29 23:00:10 +0200

    Print localstorage size

--- src/FieldPersistence.ts
+++ src/FieldPersistence.ts
@@ -20,6 +20,7 @@ export class FieldPersistence {
 
     this.localStorage.setItem(id, compressedField);
 
+    this.getLocalStorageSize();
     return;
   }
   load(id: string): Field {
@@ -96,5 +97,20 @@ export class FieldPersistence {
       )} bytes/cell`
     );
     console.log(string);
+  }
+
+  getLocalStorageSize() {
+    var _lsTotal = 0,
+      _xLen,
+      _x;
+    for (_x in this.localStorage) {
+      if (!this.localStorage.hasOwnProperty(_x)) {
+        continue;
+      }
+      _xLen = (this.localStorage[_x].length + _x.length) * 2;
+      _lsTotal += _xLen;
+      // console.log(_x.substr(0, 50) + " = " + (_xLen / 1024).toFixed(2) + " KB");
+    }
+    console.log("Local storage size used: " + (_lsTotal / 1024).toFixed(2) + " KB");
   }
 }
